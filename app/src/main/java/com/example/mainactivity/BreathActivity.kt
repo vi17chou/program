08386eeb008c99cc623d8e40b2a5d06119dee5ce
vi17chou <120084375+vi17chou@users.noreply.github.com>
2023-03-20 18:47:17 +0800

    help

--- app/src/main/java/com/example/mainactivity/BreathActivity.kt
+++ app/src/main/java/com/example/mainactivity/BreathActivity.kt
@@ -7,5 +7,6 @@ class BreathActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_breath)
+
     }
 }
