a51874c81a1ce161d5218d83b9587a627fbf12c2
vi17chou <120084375+vi17chou@users.noreply.github.com>
2023-03-27 20:58:00 +0800

    new 0327

---
+++ app/src/main/java/com/example/mainactivity/SQlite2.java
@@ -0,0 +1,14 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SQlite2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sqlite2);
+    }
+}
