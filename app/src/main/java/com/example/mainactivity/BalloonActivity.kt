71534f54e2a1d7a74c0dd353200da1953e139d22
Lai-tw <120080188+Lai-tw@users.noreply.github.com>
2023-03-29 20:55:00 +0800

    why0329

--- app/src/main/java/com/example/mainactivity/BalloonActivity.kt
+++ app/src/main/java/com/example/mainactivity/BalloonActivity.kt
@@ -7,6 +7,6 @@ class BalloonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_balloon)
-//1232223
+//1232223快瘋了啦
     }
 }
